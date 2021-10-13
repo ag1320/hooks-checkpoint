333e790cb3ef21cee5f325c64a2896827db2ad42
Aaron Gettemy <aarongettemy@gmail.com>
2021-10-13 13:51:24 -0700

    Added mo' style

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,27 +1,29 @@
 import Details from "./Details";
 import Pic from "./Pic";
-import { Card, Grid, CardContent} from '@mui/material/'
+import { Card, Grid, CardContent, CardActionArea} from '@mui/material/'
 import './ProductList.css'
 
 
 const ProductList = ({products, details, pic}) => {
     let listProducts = products.map((product, index) => (
         <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Card variant = 'outlined'>
-                <CardContent style={{ backgroundColor: "lightgrey" }}>
-                    <Grid container spacing = {4} justifyContent='center'>
-                        <Grid item>
-                            <h2 data-key={index} key = {index} className = 'header'>
-                            {product.name}
-                            </h2>
+            <Card variant = 'outlined' raised = {true} >
+                <CardContent style={{ backgroundColor: "lightgrey"}}>
+                    <CardActionArea>
+                        <Grid container spacing = {4} justifyContent='center'>
+                            <Grid item>
+                                    <h2 data-key={index} key = {index} className = 'header'>
+                                    {product.name}
+                                    </h2>
+                            </Grid>
                         </Grid>
+                    </CardActionArea>
                         <Grid item>
                             <Details details = {details} id = {index}/>
                         </Grid>
                         <Grid item>
                             <Pic pic = {pic} id = {index}/>
                         </Grid>
-                    </Grid>
                 </CardContent>
             </Card>
         </Grid>
